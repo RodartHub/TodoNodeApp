bfa5560daf592651eac9edd7f8b7b62e4e410fea
RodartHub <rodrigovalencia95@hotmail.com>
2023-09-15 09:53:03 -0500

    First commit

---
+++ app.js
@@ -0,0 +1,71 @@
+require('colors');
+
+const { inquirerMenu,
+        pausa,
+        leerInput,
+        listadoTareasBorrar,
+        confirmar,
+        mostrarListadoChecklist
+    } = require('./helpers/inquirer');
+
+const Tareas = require('./models/tareas');
+
+const { guardarDB, leerDb } = require('./helpers/guardarArchivo');
+
+
+
+console.clear();
+const main = async() => {
+
+    let opt = '';
+    const tareas = new Tareas();
+
+    const tareasDB = leerDb();
+
+    if ( tareasDB ) {
+        tareas.cargarTareasFromArray( tareasDB );
+    }
+
+    do{
+        opt = await inquirerMenu();
+        switch (opt) {
+            case '1':
+                const desc = await leerInput('Descripción: ');
+                tareas.crearTarea( desc );
+                break;
+            case '2':
+                tareas.listadoCompleto();
+                break;
+            case '3':
+                tareas.listarPendientesCompletadas( true );
+                break;
+            case '4':
+                tareas.listarPendientesCompletadas( false );
+                break;
+            case '5':
+                const ids = await mostrarListadoChecklist( tareas.listadoArr );
+                tareas.toggleCompletadas( ids );
+
+            case '6':
+                const id = await listadoTareasBorrar( tareas.listadoArr );
+                if ( id !== '0' ) {
+                    const ok = await confirmar('¿Está seguro?');
+                    if ( ok ) {
+                        tareas.borrarTarea( id );
+                        console.log('Tarea borrada');
+                    };
+                    break
+                }
+                 
+            
+        }
+
+        guardarDB( tareas.listadoArr );
+
+        await pausa();
+
+    } while (opt !== '0');
+
+};
+
+main();
